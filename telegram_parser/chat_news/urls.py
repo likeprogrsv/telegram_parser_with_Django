19aaa276d1db78d537af816c46a2531bc24afdad
progrsv <likeprogrsv@gmail.com>
2023-02-12 21:11:02 +0600

    API done

--- telegram_parser/chat_news/urls.py
+++ telegram_parser/chat_news/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from . import views
+from .views import show_news, TagList, PostList, PostByTag
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path('', views.show_news, name='news'),
+    path('', show_news, name='news'),
+    path('api/v1/tags/', TagList.as_view(), name='tag_list'),
+    path('api/v1/posts/', PostList.as_view(), name='post_list'),
+    path('api/v1/posts/<str:tag>/', PostByTag.as_view(), name='post_by_tag'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- telegram_parser/chat_news/views.py
+++ telegram_parser/chat_news/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from .models import Tag, Post, Picture
+from rest_framework import generics
+from .serializers import PostSerializer, TagSerializer
 
 
-def show_news(request):    
+class TagList(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-post_time')
+    serializer_class = PostSerializer
+
+
+class PostByTag(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        return Post.objects.filter(tag__name=tag).order_by('-post_time')
+
+
+def show_news(request):
     tags = Tag.objects.all()
     curr_tag = request.GET.get('curr_tag')
     if curr_tag is None:
